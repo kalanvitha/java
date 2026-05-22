b0fd338b588caaa3e90a8b9f818de48d205750b1
kalanvitha Gunurathi <kalanvithagundrathi@gmail.com>
2026-05-22 19:52:01 +0530

    largest of three numbers

---
+++ largestofthreenumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class largestofthreenumber {
+    public static void main(String[] args){
+        int a,b,c;//declare
+        System.out.println("enter a,b,c values to compare");
+        Scanner sc=new Scanner(System.in);//taking input from user
+        a=sc.nextInt();//input from user storing in a and b
+        b=sc.nextInt();
+        c=sc.nextInt();
+        if(a>b && a>c){
+            System.out.println("a is largest number "+a );
+        }
+        else if(b>c && b>a){
+            System.out.println("b is largest number "+b);
+        }
+        else{
+            System.out.println("c is largest number "+c);
+        }
+            sc.close();
+        }
+        }
+        
+
